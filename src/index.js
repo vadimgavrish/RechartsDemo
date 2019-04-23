52343e58fcba47a64f6a3f59f6dd71601df0dc11
vadim <vadim@gavrish.ca>
2019-04-22 22:24:34 -0400

    comments

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
+// Import all dependancies
 import React from 'react';
 import ReactDom from 'react-dom';
 
+// Create component
 const Index = () => {
   return <div>Hello</div>;
 };
 
+// Render the component to index div
 ReactDom.render(<Index />, document.getElementById('index'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
+// Import all dependancies
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
+// instantiate the plugin
 const htmlPlugin = new HtmlWebPackPlugin({
-  template: './src/index.html',
-  filename: './index.html'
+  template: './src/index.html',             // Input
+  filename: './index.html'                  // Output
 });
 
 module.exports = {
@@ -12,6 +14,7 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
+          // Use babel to transpile ES6 into Es5
           loader: 'babel-loader'
         }
       }
